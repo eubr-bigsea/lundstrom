cd1e17813b768c49bd1650010af7634475195eeb
Túlio Braga <tuliobragam@gmail.com>
2017-04-23 16:03:26 -0300

    excluding directories from logpath listdir

--- util.py
+++ util.py
@@ -17,21 +17,24 @@ def datetime_to_unix_timestamp(datetime_str):
 	uts = time.mktime(datetime.datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S,%f").timetuple())
 	return uts
 
-def sub_datetimes(end_date, start_date):
+def str_to_datetime(date_str):
 	from datetime import datetime
+	return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S,%f')
 
-	def __datetime(date_str):
-	    return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S,%f')
-
-	start = __datetime(start_date)
-	end = __datetime(end_date)
+def sub_str_datetimes(end_date, start_date):
+	start = str_to_datetime(start_date)
+	end = str_to_datetime(end_date)
 
 	delta = end - start
 	return delta.total_seconds()*1000.0
 
+def sub_datetimes(end_date, start_date):
+	delta = end_date - start_date
+	return delta.total_seconds()*1000.0
+
 def read_files(logpath):
 	import os
-	return [s for s in os.listdir(logpath) if not s.startswith('.DS_Store')]
+	return [s for s in os.listdir(logpath) if not s.startswith('.DS_Store') and not os.path.isdir(logpath + s)]
 	#return [s for s in os.listdir(logpath) if s.startswith('app') or s.startswith('eventLogs')]
 
 def read_file(filepath):
