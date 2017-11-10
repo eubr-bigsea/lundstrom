3c213cf9548f497d0990228e17f8c84499b545c3
Túlio Braga <tuliobragam@gmail.com>
2017-11-10 00:55:43 -0200

    adding -s param to print stages + addig -l param to configure logs dir

--- run.py
+++ run.py
@@ -3,17 +3,25 @@ import os, sys, json, getopt
 def main(argv):
 	# Getting params
 	try:
-	  	opts, args = getopt.getopt(argv,"bn:c:r:d:q:p:")
+	  	opts, args = getopt.getopt(argv,"bn:c:r:d:q:p:l:sn")
 	except getopt.GetoptError:
 	  	print 'python run.py -n <nodes> -c <cores> -r <ram> -d <data> -q <query> -p <platform> -f <conffile> -b'
+		print 'or'
+		print 'python run.py -l <logdir> -p <platform>'
 	  	sys.exit(2)
 
 	config_file = False
 	baseConfig = False
 	cores_to_predict = False
+	confdir = False
 
 	# iterating over the params
+	printStages = False
 	for opt, value in opts:
+		if opt == "-s":
+			printStages = True
+		if opt == "-l":
+			confdir = value
 		if opt == "-n":
 			num_nodes = value
 		if opt == "-c":
@@ -34,26 +42,34 @@ def main(argv):
 	# input parameters - to predict
 	data = {"nodes": num_nodes, "cores": num_cores, "ram": ram_size, "data": datasize, "query": query}
 
-	# reading config
-	if not config_file:
-		config_file = os.path.dirname(os.path.realpath(__file__)) + "/config.json"
+	if confdir == False:
+		# reading config
+		if not config_file:
+			config_file = os.path.dirname(os.path.realpath(__file__)) + "/config.json"
 
-	config = json.loads(open(config_file, 'r').read())
+		config = json.loads(open(config_file, 'r').read())
 
-	# executing models
+		if platform == "compss":
+			# determining log dir
+			confdir = str(config["COMPSS_LOG_DIR"])
+		elif platform == "spark":
+			# determining log dir
+			confdir = str(config["SPARK_LOG_DIR"])
+		else:
+			print "ERROR: Only 'compss' and 'spark' are available. Please, type the platform name in lowercase."
+			sys.exit()
+
 	if platform == "compss":
 		from run_compss import run_model
-		# determining log dir
-		confdir = str(config["COMPSS_LOG_DIR"])
 	elif platform == "spark":
 		from run_spark import run_model
-		# determining log dir
-		confdir = str(config["SPARK_LOG_DIR"])
-	else:
-		print "ERROR: Only 'compss' and 'spark' are available. Please, type the platform name in lowercase."
-		sys.exit()
 
+	# executing model
 	result = run_model(data, cores_to_predict, confdir)
+
+	if printStages == False:
+		result.pop('stages', None)
+
 	print result
 	sys.exit()
 
